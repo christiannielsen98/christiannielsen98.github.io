06df7e4dff1e249ecd0404daebb830f3ef1747ba
christiannielsen98 <56675603+christiannielsen98@users.noreply.github.com>
2021-01-10 23:46:15 +0100

    andet plot

--- docs/Python/visualization.py
+++ docs/Python/visualization.py
@@ -1,24 +1,88 @@
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-
 HR_df = pd.read_csv("/Users/christiannielsen/Documents/GitHub/christiannielsen98.github.io/HRDataset_v14.csv")
-print(HR_df)
 
-def perfcorr():
 
-    fig = px.bar(HR_df, x="ManagerName", y="EmpID", color="PerformanceScore", barmode="group",
-    #              facet_row="time", facet_col="PerformanceScore",
-    #              #category_orders={"day": ["Thur", "Fri", "Sat", "Sun"],
-    #                               "time": ["Lunch", "Dinner"]})
+def PerfomanceManagerCorrelation():
+    fig = px.bar(HR_df, x="ManagerName", y="EmpID", color="PerformanceScore")
+    fig.update_layout(barmode='stack', xaxis={'categoryorder': 'total descending'})
+    return fig.write_html("/Users/christiannielsen/Documents/GitHub/christiannielsen98.github.io/docs/Python/HTML/PerfomanceManagerCorrelation.html")
+PerfomanceManagerCorrelation()
+
+def EquitablePayByGroups():
+    trace_position_m = go.Box(x=HR_df.loc[HR_df['Sex'] == "M "].Position,
+                            y=HR_df.loc[HR_df['Sex'] == "M "].Salary,
+                            name='Men',
+                            marker_color='blue'
+                            )
+
+    trace_position_f = go.Box(x=HR_df.loc[HR_df['Sex'] == "F"].Position,
+                            y=HR_df.loc[HR_df['Sex'] == "F"].Salary,
+                            name='Women',
+                            marker_color='red'
+                            )
+
+    trace_race_m = go.Box(x=HR_df.loc[HR_df['Sex'] == "M "].RaceDesc,
+                              y=HR_df.loc[HR_df['Sex'] == "M "].Salary,
+                              name='Men',
+                              marker_color='blue'
+                          )
+    trace_race_f = go.Box(x=HR_df.loc[HR_df['Sex'] == "F"].RaceDesc,
+                              y=HR_df.loc[HR_df['Sex'] == "F"].Salary,
+                              name='Women',
+                              marker_color= 'red'
+                          )
+
+    trace_maritalstatus_m = go.Box(x=HR_df.loc[HR_df['Sex'] == "M "].MaritalDesc,
+                          y=HR_df.loc[HR_df['Sex'] == "M "].Salary,
+                          name='Men',
+                          marker_color='blue'
+                                   )
+    trace_maritalstatus_f = go.Box(x=HR_df.loc[HR_df['Sex'] == "F"].MaritalDesc,
+                          y=HR_df.loc[HR_df['Sex'] == "F"].Salary,
+                          name='Women',
+                          marker_color='red'
+                                   )
+
+    traces = [trace_position_m, trace_position_f, trace_race_m, trace_race_f, trace_maritalstatus_m, trace_maritalstatus_f]
+
+
+    updatemenus = list([
+        dict(active=0,
+             buttons=list([
+                 dict(label='Position',
+                      method='update',
+                      args=[{'visible': [True, True, False, False, False, False]},
+                            {'title': 'Salary for every position'}]),
+                 dict(label='Race',
+                      method='update',
+                      args=[{'visible': [False, False, True, True, False, False]},
+                            {'title': 'Salary for every race'}]),
+                 dict(label='Marital status',
+                      method='update',
+                      args=[{'visible': [False, False, False, False, True, True]},
+                            {'title': 'Salary for every marital status'}]),
+             ]),
+             )
+    ])
+
+    layout = dict(title='Salary pay', showlegend=True,
+                  updatemenus=updatemenus,
+                  yaxis_title='Yearly salary in dollars',
+                  boxmode='group',
+                  xaxis_type='category'
                   )
 
+    fig = go.Figure(data=traces, layout=layout)
+    fig.update_xaxes(showgrid=True)
 
-    #fig = go.Figure(go.Bar(x=HR_df.ManagerName, y= HR_df.PerformanceScore.str.contains('PIP'), name='PIP'))
+    fig.show()
+    return fig.write_html(
+        "/Users/christiannielsen/Documents/GitHub/christiannielsen98.github.io/docs/Python/HTML/EquitablePayByGroups.html")
 
-    fig.update_layout(barmode='stack', xaxis={'categoryorder': 'total descending'})
-    # fig.show()
-    fig.write_html("/Users/christiannielsen/Documents/GitHub/christiannielsen98.github.io/docs/Python/HTML/PerfomanceManagerCorrelation.html")
+EquitablePayByGroups()
 
-perfcorr()
-
+def Heatmap():
+    fig = px.density_heatmap(HR_df, x="Position", y="RaceDesc")
+    fig.show()
--- index.md
+++ index.md
@@ -3,10 +3,16 @@
 ## Is there any relationship between who a person works for and their performance score?
 - [Bar](https://christiannielsen98.github.io/docs/Python/HTML/PerfomanceManagerCorrelation.html)
 
+## Are there areas of the company where pay is not equitable?
+- [Boxplot](https://christiannielsen98.github.io/docs/Python/HTML/EquitablePayByGroups.html)
+
 ## What is the overall diversity profile of the organization?
+- 
 
 ## What are our best recruiting sources if we want to ensure a diverse organization?
+- 
 
-## Can we predict who is going to terminate and who isn't? What level of accuracy can we achieve on this?
+## Can we predict who is going to be terminated and who isn't? What level of accuracy can we achieve on this?
+- 
 
-## Are there areas of the company where pay is not equitable?
+
